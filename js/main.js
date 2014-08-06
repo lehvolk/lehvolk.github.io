0f204080841d41218256853c0f17a22f1edb9246
lenvolk <pulyadura1>
2014-08-06 18:12:46 +0400

    - development workflow

--- js/main.js
+++ js/main.js
@@ -1,46 +1,3 @@
-(function () {
-
-    var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function () {
-        var html = $(this).parent().html();
-        html = cleanSource(html);
-        $("#source-modal pre").text(html);
-        $("#source-modal").modal();
-    });
-
-    $('.bs-component [data-toggle="popover"]').popover();
-    $('.bs-component [data-toggle="tooltip"]').tooltip();
-
-    $(".bs-component").hover(function () {
-        $(this).append($button);
-        $button.show();
-    }, function () {
-        $button.hide();
-    });
-
-    function cleanSource(html) {
-        var lines = html.split(/\n/);
-
-        lines.shift();
-        lines.splice(-1, 1);
-
-        var indentSize = lines[0].length - lines[0].trim().length,
-            re = new RegExp(" {" + indentSize + "}");
-
-        lines = lines.map(function (line) {
-            if (line.match(re)) {
-                line = line.substring(indentSize);
-            }
-
-            return line;
-        });
-
-        lines = lines.join("\n");
-
-        return lines;
-    }
-
-})();
-
 var docs = angular.module('docs', ['ngRoute']);
 
 docs.config(['$routeProvider',
@@ -52,11 +9,9 @@ docs.config(['$routeProvider',
             }).
             when('/quantile/guide', {
                 templateUrl: 'quantile/guide.html'
-
             }).
             when('/quantile/download', {
                 templateUrl: 'quantile/downloads.html'
-
             }).
             when('/toolkits', {
                 templateUrl: 'toolkits/toolkits.html'
@@ -67,8 +22,27 @@ docs.config(['$routeProvider',
             });
     }]);
 
-function scrollTo(id) {
-    $('html,body').animate({
-        scrollTop: $("#" + id).offset().top - 80
-    });
-}
+docs.controller('PageCtrl', function ($scope, $location, $anchorScroll) {
+    $scope.scrollTo = function (id) {
+        $location.hash(id);
+        $anchorScroll();
+    }
+});
+
+docs.directive('scrollOnClick', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, $elm, attrs) {
+            var idToScroll = attrs.destination;
+            $elm.on('click', function () {
+                var $target;
+                if (idToScroll) {
+                    $target = $("#" + idToScroll);
+                } else {
+                    $target = $elm;
+                }
+                $("html,body").animate({scrollTop: $target.offset().top - 80});
+            });
+        }
+    }
+});
